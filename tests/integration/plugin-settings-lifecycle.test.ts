231b976508258338732ffeb59dfb48229a49e6d6
NeoMei <neomei@neomei-macbook-black.taile852e2.ts.net>
2026-08-24 20:40:33 +0800

    release: prepare AgentWiki Sync 0.2.9

--- tests/integration/plugin-settings-lifecycle.test.ts
+++ tests/integration/plugin-settings-lifecycle.test.ts
@@ -100,8 +100,8 @@ async function makePlugin(input: {
   const plugin = new AgentWikiSyncPlugin(app as never, {
     id: "agentwiki-sync",
     name: "AgentWiki Sync",
-    version: "0.2.8",
-    minAppVersion: "1.11.4",
+    version: "0.2.9",
+    minAppVersion: "1.11.5",
     description: "",
     author: "NeoMei",
   });
